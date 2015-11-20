36b02a6807f1a563448163622e91ef2e7cfc635f
Jens Lind <jens.lind@oddhill.se>
2015-11-20 19:19:48 +0100

    Added ghreleases v2

--- index.js
+++ index.js
@@ -3,6 +3,14 @@
 const electron = require('electron')
 const app = electron.app
 const BrowserWindow = electron.BrowserWindow
+const GhReleases = require('./GhReleases')
+
+let options = {
+  repo: 'jenslind/electron-gh-releases-test-v2',
+  version: app.getVersion()
+}
+
+let updater = new GhReleases(options)
 
 let mainWindow = null;
 
@@ -15,4 +23,15 @@ app.on('window-all-closed', function () {
 app.on('ready', function () {
   mainWindow = new BrowserWindow({width: 800, height: 600})
   mainWindow.loadURL('file://' + __dirname + '/index.html')
+  mainWindow.webContents.openDevTools()
+
+  updater.check(function (err, status) {
+    if (!err && status) {
+      updater.download()
+    }
+  })
+
+  updater.on('update-downloaded', function () {
+    updater.install()
+  })
 })
