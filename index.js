df2a0d3d33b496e0b98a4fd37dead0137ff85ed9
Jens Lind <jens.lind@oddhill.se>
2015-11-20 19:07:26 +0100

    Init test app

---
+++ index.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const electron = require('electron')
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+
+let mainWindow = null;
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('ready', function () {
+  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow.loadURL('file://' + __dirname + '/index.html')
+})
